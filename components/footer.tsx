2035a083bffee7a6c3cd2fc71dd92a98c6f6742f
riddhimanrana <riddhiman.rana@gmail.com>
2025-05-25 17:48:23 -0700

    update footer for minor fix

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,22 +1,22 @@
-"use client"
-import React, { useEffect, useState } from "react"
+"use client";
+import React, { useEffect, useState } from "react";
 
 const Footer: React.FC = () => {
-  const [mounted, setMounted] = useState(false)
+  const [mounted, setMounted] = useState(false);
 
   useEffect(() => {
-    setMounted(true)
-  }, [])
+    setMounted(true);
+  }, []);
 
-  if (!mounted) return null
+  if (!mounted) return null;
 
   return (
-    <footer className="pb-8">
-      <div className="text-center text-sm text-gray-600 dark:text-gray-400">
+    <footer className="pb-8 bg-white dark:bg-gray-950">
+      <div className="text-center bg-text-sm text-gray-600 dark:text-gray-400">
         © {new Date().getFullYear()} Riddhiman Rana. All rights reserved.
       </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
